93f388931431e2c5d42c99ce1179a0f20cf3fa51
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-08-30 00:24:15 +0200

    rhine: add anc firmware synmlink

--- Android.mk
+++ Android.mk
@@ -2,4 +2,12 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_VENDOR_PLATFORM),rhine)
     include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+    $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wcd9320; \
+        ln -sf /data/misc/audio/wcd9320_anc.bin \
+        $(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_anc.bin; \
+        ln -sf /data/misc/audio/wcd9320_mad_audio.bin \
+        $(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_mad_audio.bin; \
+        ln -sf /data/misc/audio/mbhc.bin \
+        $(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_mbhc.bin)
 endif
