758920b7051b2f6ba8e411c957346b079ced696a
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-10-22 12:58:42 +0200

    rhine: cleanup recovery stuff
    Change-Id: I4165d43d4f7d8afe9dc65ec7e5de3f1cadef9335

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -98,7 +98,6 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/
 # Custom boot
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 BOARD_CUSTOM_BOOTIMG_MK := device/sony/rhine-common/custombootimg.mk
-BOARD_CUSTOM_GRAPHICS := ../../../device/sony/rhine-common/recovery/graphics.c
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 
 TARGET_RECOVERY_FSTAB := device/sony/rhine-common/rootdir/fstab.qcom
