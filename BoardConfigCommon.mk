3307396e54abf0914afd7fff865854d6eb8f475b
Ethan Chen <intervigil@gmail.com>
2014-03-29 17:06:20 -0700

    rhine: Reorganize board config, package config
    Change-Id: I85e070adc85b0625652bbee4d99b0005135c868c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -30,8 +30,6 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8974
 TARGET_BOARD_PLATFORM := msm8974
 BOARD_VENDOR_PLATFORM := rhine
 
-BOARD_LIB_DUMPSTATE := libdumpstate.sony
-
 # Architecture
 TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_CPU_VARIANT := krait
@@ -52,11 +50,65 @@ BOARD_KERNEL_SEPARATED_DT := true
 
 # Qualcomm
 BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QC_TIME_SERVICES := true
 TARGET_USES_QCOM_BSP := true
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 TARGET_QCOM_AUDIO_VARIANT := caf
 TARGET_QCOM_DISPLAY_VARIANT := caf-new
 TARGET_QCOM_MEDIA_VARIANT := caf-new
+
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+AUDIO_FEATURE_DISABLED_USBAUDIO := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+BLUETOOTH_HCI_USE_MCT := true
+
+# Camera
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
+BOARD_CAMERA_HAVE_ISO := true
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+
+# CM Hardware
+BOARD_HARDWARE_CLASS := device/sony/rhine-common/cmhw
+
+# Dumpstate
+BOARD_LIB_DUMPSTATE := libdumpstate.sony
+
+# GPS
+TARGET_PROVIDES_GPS_LOC_API := true
+
+# Graphics
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_rhine
+
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+    device/sony/rhine-common/sepolicy
+
+# The list below is order dependent
+BOARD_SEPOLICY_UNION := \
+    device.te \
+    app.te \
+    file_contexts
 
 # Wifi
 BOARD_HAS_QCOM_WLAN              := true
@@ -73,63 +125,15 @@ WIFI_DRIVER_FW_PATH_AP           := "ap"
 
 BOARD_USE_SONY_MACUPDATE := true
 
-# Shader cache config options
-# Maximum size of the  GLES Shaders that can be cached for reuse.
-# Increase the size if shaders of size greater than 12KB are used.
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-
-# Maximum GLES shader cache size for each app to store the compiled shader
-# binaries. Decrease the size if RAM or Flash Storage size is a limitation
-# of the device.
-MAX_EGL_CACHE_SIZE := 2048*1024
-
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
-
-BOARD_SEPOLICY_DIRS += \
-    device/sony/rhine-common/sepolicy
-
-# The list below is order dependent
-BOARD_SEPOLICY_UNION := \
-    device.te \
-    app.te \
-    file_contexts
-
-# Audio
-BOARD_USES_ALSA_AUDIO := true
-AUDIO_FEATURE_DISABLED_USBAUDIO := true
-
-# Camera
-USE_DEVICE_SPECIFIC_CAMERA := true
-BOARD_CAMERA_HAVE_ISO := true
-
-# GPS
-TARGET_PROVIDES_GPS_LOC_API := true
-
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_QCOM := true
-BLUETOOTH_HCI_USE_MCT := true
-
-# Time
-BOARD_USES_QC_TIME_SERVICES := true
-
-# Pre KK blob compat
-COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
-
-BOARD_CHARGER_ENABLE_SUSPEND := true
-
-BOARD_HARDWARE_CLASS := device/sony/rhine-common/cmhw
-
-# Custom boot
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
-
-TARGET_RECOVERY_FSTAB := device/sony/rhine-common/rootdir/fstab.qcom
-
+# Filesystem
 BOARD_FLASH_BLOCK_SIZE := 131072
-BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_USERIMAGES_USE_EXT4 := true
+
+# Recovery
+TARGET_RECOVERY_FSTAB := device/sony/rhine-common/rootdir/fstab.qcom
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 
 # TWRP flags
 DEVICE_RESOLUTION := 1080x1920
@@ -154,7 +158,5 @@ TW_BRIGHTNESS_PATH := /sys/class/leds/wled:backlight/brightness
 TW_MAX_BRIGHTNESS := 4095
 TW_NO_USB_STORAGE := true
 
+# Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := device/sony/rhine-common
-
-# Vendor Init
-TARGET_INIT_VENDOR_LIB := libinit_rhine
