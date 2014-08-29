f6532ab0d8e31a42b7a343bb7989694dba6614dc
Chirayu Desai <cdesai@cyanogenmod.org>
2014-08-29 10:05:06 +0530

    audio: set fluencetype to fluence
    * Stock (and CM/AOSP audio) support persist.dualmic.config,
      but audio-caf has moved on to ro.qc.sdk.audio.fluencetype
    * Set fluencetype to fluence for the dual mic config
    
    Change-Id: I5929f92021cce667bc6cdc0139525d4ac4377798

--- rhine.mk
+++ rhine.mk
@@ -186,7 +186,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Voice Call
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qc.sdk.audio.fluencetype=none \
+    ro.qc.sdk.audio.fluencetype=fluence \
     persist.audio.dualmic.config=endfire \
     persist.audio.fluence.voicecall=true \
     persist.audio.fluence.voicerec=false \
