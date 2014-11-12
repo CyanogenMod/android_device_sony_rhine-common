1783995806c87c9556a43e20eb12e4039db08dc6
Chirayu Desai <chirayudesai1@gmail.com>
2014-11-12 16:46:05 +0530

    Inherit HWUI memory limits from frameworks/native
    * No need to duplicate it here.
    
    Change-Id: I2ec7e9f09ebefaf7a4ae76b7217919965bf83956

--- rhine.mk
+++ rhine.mk
@@ -217,19 +217,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qualcomm.sensors.georv=true \
     ro.qualcomm.sensors.smd=sony
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=72 \
-    ro.hwui.layer_cache_size=48 \
-    ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.path_cache_size=32 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=1024
-
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qualcomm.bt.hci_transport=smd
@@ -267,6 +254,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0
+
+# HWUI memory limits
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Include non-opensource parts
 $(call inherit-product, vendor/sony/rhine-common/rhine-common-vendor.mk)
