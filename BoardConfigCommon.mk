63c2a0763ef6b4f186ce34f42abac09455a43798
david <davidteri91@gmail.com>
2014-11-21 23:40:54 +0100

    rhine-common: Include an expanded selection of fonts
    Change-Id: I0c83bc86226148f7b410919fb3ed4ec874927cfc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -128,3 +128,6 @@ BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := device/sony/rhine-common
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
