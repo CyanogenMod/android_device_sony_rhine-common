8f898730361e42ae48ca8df6a7ed4b68abaf2512
david <davidteri91@gmail.com>
2014-12-10 09:18:16 +0100

    rhine: remove cyanogen dsp
    Change-Id: I0c29bd81e5e223a25363d3a3d80e228a52472bd4

--- rhine.mk
+++ rhine.mk
@@ -84,7 +84,6 @@ PRODUCT_PACKAGES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf
 
 # Display
