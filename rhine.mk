f4b79b9cd799c05cc71e89211e22b4013f9e8d73
David Viteri <davidteri91@gmail.com>
2015-05-15 09:13:37 +0200

    rhine: init: create common usb.rc
    Following AOSP
    
    https://github.com/sonyxperiadev/device-sony-rhine/blob/master/rootdir/init.shinano.usb.rc
    
    Fix value
    /sys/class/android_usb/android0/f_rndis/vendorID
    
    Change-Id: I2cf6613e0aa887755da8061a3a7fea7b08b4b946
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- rhine.mk
+++ rhine.mk
@@ -45,6 +45,7 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
     $(COMMON_PATH)/rootdir/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
     $(COMMON_PATH)/rootdir/system/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
+    $(COMMON_PATH)/rootdir/init.sony.usb.rc:root/init.sony.usb.rc \
     $(COMMON_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc
 
 # Recovery
