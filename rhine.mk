80abfe77921106eacc6fd2c008540ed7c58e0892
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2015-09-14 13:52:31 +0200

    rhine: Use the new power modes configuration
    Change-Id: Icc7734446e49b2af6ec7a0e2d59eb7e06e0961c6

--- rhine.mk
+++ rhine.mk
@@ -43,6 +43,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
     $(COMMON_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
+    $(COMMON_PATH)/rootdir/init.qcom.power.rc:root/init.qcom.power.rc \
     $(COMMON_PATH)/rootdir/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
     $(COMMON_PATH)/rootdir/system/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
     $(COMMON_PATH)/rootdir/init.sony.usb.rc:root/init.sony.usb.rc \
@@ -218,6 +219,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # MDP
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.mdpcomp.logs=0
+
+# IO Scheduler
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.io.scheduler=row
 
 # Time
 PRODUCT_PROPERTY_OVERRIDES += \
