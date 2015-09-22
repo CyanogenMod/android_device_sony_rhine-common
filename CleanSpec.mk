7ac83425dcf767d5116d943c3b6aca1dd0a9ce9f
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2015-09-22 14:36:58 +0200

    rhine: update prima stuff
    Change-Id: Id553d2ea1e6e9ecab5c352a8c6b32c30ec76a87e

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -55,7 +55,6 @@ $(call add-clean-step, find $(PRODUCT_OUT) -name "*.prop" -print0 | xargs -0 rm 
 $(call add-clean-step, find $(OUT_DIR) -name "wpa_supplicant*" -print0 | xargs -0 rm -f)
 $(call add-clean-step, find $(PRODUCT_OUT) -name "*.img" -print0 | xargs -0 rm -f)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/SHARED_LIBRARIES/libhardware_legacy_intermediates/)
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib/modules/wlan.ko)
 $(call add-clean-step, rm -f $(PRODUCT_OUT)/root/default.prop)
 $(call add-clean-step, rm -f $(PRODUCT_OUT)/recovery/root/default.prop)
 $(call add-clean-step, rm -f $(PRODUCT_OUT)/system/build.prop)
