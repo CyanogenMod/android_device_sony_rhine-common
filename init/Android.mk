f779a7a235ea87c6567384db86178b5d9a29fdc0
srisurya95 <srisurya95@gmail.com>
2015-10-19 07:22:53 +0200

    rhine-common: Build init as C++
    Change-Id: Ie6212aa11d542a3b42a6be0ac21e48d1b400c177

--- init/Android.mk
+++ init/Android.mk
@@ -6,7 +6,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall
-LOCAL_SRC_FILES := init_rhine.c
+LOCAL_SRC_FILES := init_rhine.cpp
 LOCAL_MODULE := libinit_rhine
 include $(BUILD_STATIC_LIBRARY)
 
