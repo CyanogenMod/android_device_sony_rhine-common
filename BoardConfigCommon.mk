2f38b9e50e9614759f09346c0559a92815fdc129
Chirayu Desai <chirayudesai1@gmail.com>
2015-11-16 16:31:32 +0530

    DO NOT MERGE: SELinux permissive
    Change-Id: I1ae3a2e9bb008fec24137966166fd07c4b12fae0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -24,6 +24,7 @@ BOARD_VENDOR_PLATFORM := rhine
 BOARD_KERNEL_BASE     := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE  := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3b7 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 vmalloc=300M dwc3.maximum_speed=high dwc3_msm.prop_chg_detect=Y
+BOARD_KERNEL_CMDLINE  += androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS  := --ramdisk_offset 0x02000000 --tags_offset 0x01E00000
 BOARD_KERNEL_SEPARATED_DT := true
 
